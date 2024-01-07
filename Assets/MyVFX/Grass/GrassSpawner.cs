96359a6f8f27b5a237361997cef6d207b89a59ba
Egor Ilyashenko <eg.ilyashenko@gmail.com>
2024-01-07 21:50:25 +0200

    Project upload

---
+++ Assets/MyVFX/Grass/GrassSpawner.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+namespace MyVFX.Grass
+{
+    public class GrassSpawner : MonoBehaviour
+    {
+        [SerializeField] private ComputeShader computeShader;
+        [SerializeField] private Material planeMaterial;
+    
+        private void Start()
+        {
+            
+        }
+    }
+}
